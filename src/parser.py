7b9c21ffcae9633f2bb6113775da85477ca1f352
Juan-Pablo Silva <jpsilvapena@hotmail.com>
2017-10-12 22:23:42 -0300

    Add report 2

--- src/parser.py
+++ src/parser.py
@@ -15,29 +15,30 @@ with open('general.csv', 'r', encoding='utf-8') as fa:
         anime_mapping[int(line[0])] = id
         id += 1
 
+#
+# with open('new_anime_id.csv', 'w', encoding='utf-8', newline="\n") as o1:
+#     out1 = csv.writer(o1, delimiter=',')
+#     for k, v in anime_mapping.items():
+#         out1.writerow([k, v])
 
-with open('new_anime_id.csv', 'w', encoding='utf-8', newline="\n") as o1:
-    out1 = csv.writer(o1, delimiter=',')
-    for k, v in anime_mapping.items():
-        out1.writerow([k, v])
-#
-# with open('mal_filled.csv', 'r', encoding='utf-8') as f2:
-#     with open('out_file.csv', 'w', encoding='utf-8') as o:
-#
-#         ff2 = csv.reader(f2, delimiter=',')
-#         out = csv.writer(o, delimiter=',')
-#
-#         user = 0
-#         the_list = [0] * 13511
-#         for l2 in ff2:
-#             if user == 0:
-#                 user = user_mapping.get(l2[0])
-#             if user != user_mapping.get(l2[0]):
-#                 out.writerow([user] + the_list)
-#                 the_list = [0] * 13511
-#                 user = user_mapping.get(l2[0])
-#
-#             try:
-#                 the_list[anime_mapping.get(int(l2[2]) - 1)] = l2[5]
-#             except:
-#                 continue
+with open('mal_filled.csv', 'r', encoding='utf-8') as f2:
+    with open('out_file.csv', 'w', encoding='utf-8', newline="\n") as o:
+
+        ff2 = csv.reader(f2, delimiter=',')
+        out = csv.writer(o, delimiter=',')
+
+        user = 0
+        the_list = [0] * 13511
+        for l2 in ff2:
+            if user == 0:
+                user = user_mapping.get(l2[0])
+            if user != user_mapping.get(l2[0]):
+                out.writerow([user] + the_list)
+                the_list = [0] * 13511
+                user = user_mapping.get(l2[0])
+
+            try:
+                the_list[anime_mapping.get(int(l2[2]) - 1)] = l2[5]
+            except:
+                continue
+        out.writerow([user] + the_list)
