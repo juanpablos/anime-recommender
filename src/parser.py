69b16c7d3c24038d9c878adf64abc02007313049
Juan-Pablo Silva <jpsilvapena@hotmail.com>
2017-11-19 05:10:52 -0300

    Cleaned files and structure

--- src/parser.py
+++ src/parser.py
@@ -1,54 +1,72 @@
 import csv
 
-user_mapping = {}
-anime_mapping = {}
 
-with open('users_id.csv', 'r', encoding='utf-8') as f1:
-    ff1 = csv.reader(f1, delimiter=',')
-    for l1 in ff1:
-        user_mapping[l1[1]] = l1[0]
-
-with open('general.csv', 'r', encoding='utf-8') as fa:
-    ffa = csv.reader(fa, delimiter=',')
-    id = 1
-    for line in ffa:
-        if line[0] != 'anime_id' and not anime_mapping.get(int(line[0])):
-            anime_mapping[int(line[0])] = id
-            id += 1
+def map_users(user_id_file):
+    user_mapping = {}
+    with open(user_id_file, 'r', encoding='utf-8') as f1:
+        ff1 = csv.reader(f1, delimiter=',')
+        for l1 in ff1:
+            user_mapping[l1[1]] = int(l1[0])
+    return user_mapping
 
 
-with open('new_anime_id.csv', 'w', encoding='utf-8', newline="\n") as o1:
-    out1 = csv.writer(o1, delimiter=',')
-    for k, v in anime_mapping.items():
-        out1.writerow([k, v])
-#
-with open('mal_filled.csv', 'r', encoding='utf-8') as f2:
-    with open('out_file.csv', 'w', encoding='utf-8') as o:
-#
-# with open('new_anime_id.csv', 'w', encoding='utf-8', newline="\n") as o1:
-#     out1 = csv.writer(o1, delimiter=',')
-#     for k, v in anime_mapping.items():
-#         out1.writerow([k, v])
+def map_anime(general_anime_file):
+    anime_mapping = {}
+    with open(general_anime_file, 'r', encoding='utf-8') as fa:
+        ffa = csv.reader(fa, delimiter=',')
+        next(ffa)
+        for line in ffa:
+            if anime_mapping.get(int(line[1])):
+                raise Exception("Index already exists")
+            anime_mapping[int(line[1])] = int(line[0])
+    return anime_mapping
 
-with open('mal_filled.csv', 'r', encoding='utf-8') as f2:
-    with open('out_file.csv', 'w', encoding='utf-8', newline="\n") as o:
 
-        ff2 = csv.reader(f2, delimiter=',')
-        out = csv.writer(o, delimiter=',')
+def reduce_user_lists(general_anime_file, user_id_file, user_list_file, out_file):
+    anime_mapping = map_anime(general_anime_file)
+    user_mapping = map_users(user_id_file)
 
-        user = 0
-        the_list = [0] * len(anime_mapping)
-        for l2 in ff2:
-            if user == 0:
-                user = user_mapping.get(l2[0])
-            if user != user_mapping.get(l2[0]):
-                out.writerow([user] + the_list)
-                the_list = [0] * len(anime_mapping)
-                user = user_mapping.get(l2[0])
+    with open(user_list_file, 'r', encoding='utf-8') as f2:
+        with open(out_file, 'w', encoding='utf-8', newline="\n") as o:
 
-            try:
-                the_list[anime_mapping.get(int(l2[2]) - 1)] = l2[5]
-            except:
-                continue
+            ff2 = csv.reader(f2, delimiter=',')
+            out = csv.writer(o, delimiter=',')
 
-        out.writerow([user] + the_list)
+            out_dict = {}
+
+            missing = []
+            miss = False
+
+            for l2 in ff2:
+                try:
+                    anime_id = anime_mapping.get(int(l2[2]))
+                    if not anime_id:
+                        if not miss:
+                            miss = True
+                        if not int(l2[2]) in missing:
+                            missing.append(int(l2[2]))
+                            print(int(l2[2]))
+                        continue
+                except:
+                    raise Exception("Id not found in map")
+
+                if isinstance(out_dict.get(user_mapping.get(l2[0])), list):
+                    out_dict[user_mapping.get(l2[0])].append((anime_id, int(l2[5])))
+                else:
+                    out_dict[user_mapping.get(l2[0])] = [(anime_id, int(l2[5]))]
+
+            for k, v in out_dict.items():
+                out.writerow([k] + v)
+
+            if miss:
+                missing.sort()
+                print(missing)
+                print(len(missing))
+                with open('missing_ids.txt', 'w', newline="\n") as mi:
+                    for m in missing:
+                        mi.write(str(m) + "\n")
+
+
+if __name__ == "__main__":
+    reduce_user_lists('../Data/general.csv', '../Data/users_id.csv', '../Data/mal_filled.csv',
+                      '../user_interactions.csv')
